c7b07dccf0508bb8221e4495cee27269324782bb
Megan Campbell <megancampbell201@gmail.com>
2022-03-01 11:19:55 -0500

    added 404 error message

--- server.js
+++ server.js
@@ -44,6 +44,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
 // add route and send response
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -51,6 +56,15 @@ app.get('/api/animals', (req, res) => {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
   });
 
 // make server listen
